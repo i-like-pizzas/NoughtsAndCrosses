a1fc0e2c83bb5041621a0cd1625b3753acefae1c
Your Name <you@example.com>
2024-07-07 14:21:07 +0100

    DI

---
+++ ApplicationServices/Services/IGameService.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationServices.DTO;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationServices.Services
+{
+    public interface IGameService
+    {
+        PlayResult Play(PlayRequest request);
+
+        string GetInstructions();
+    }
+}
